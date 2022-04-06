dd87749147f977fd4a03fbd498dc33f81a4ea6fd
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2022-04-06 01:19:16 -0400

    updated code
    The previous information in here was confusing when I lost my way in class. I went ahead and appended the new code so that it makes sense and isn't a carbon copy of the previous attempt/information from my user.py assignment.

--- bank_account.py
+++ bank_account.py
@@ -1,53 +1,63 @@
-# class User:
-#     def __init__(self,name):
-#         self.name = name
-#         self.amount = 0
-#     # adding the deposit method
+class BankAccount:
+    bank_name = "Doja Bank" #pulled from learn platform under @classmethod 
+    all_accounts = [] #pulled from learn platform under @classmethod
+    # don't forget to add some default values for these parameters!
+    def __init__(self, balance, int_rate): 
+        self.balance = balance
+        self.int_rate = int_rate
+    def deposit(self, amount):
+        self.balance += (amount)
+        return self
+    def withdrawl(self, amount):
+        self.balance -= (amount)
+        return self
+    def display_account_info(self):
+        print(f"BankAccount: ${self.balance}")
+        return self
+    def yield_interest(self):
+        self.balance = self.balance + (self.balance * self.int_rate) #tried to use yield_interest instead, but realized -> I should be calling what's in the __init__ for the program instead. - was yeilding float/method errors
+        return self
 
-#     def make_deposit(self,amount):
-#         self.amount += amount
+adamAccount = BankAccount(500, .15)
+jasonAccount = BankAccount(2000, .05)
 
-#     def make_withdrawal(self,amount):
-#         self.amount -= amount
-
-#     def display_user_balance(self):
-#         print(f"User: {self.name}, Balance: ${self.amount}")
-
-#     def transfer_money(self,amount,user):
-#         self.amount -= amount
-#         user.amount += amount
-#         self.display_user_balance()
-#         user.display_user_balance()
-
-# Waldo = User('Waldo')
-# Mickey = User('Mickey Mouse')
-# Minnie = User('Minnie Mouse')
-
-# Waldo.make_deposit(100)
-# Waldo.make_deposit(200)
-# Waldo.make_deposit(50)
-# Waldo.make_withdrawal(45)
-# Waldo.display_user_balance()
-
-# Mickey.make_deposit(1000)
-# Mickey.make_deposit(1000)
-# Mickey.make_withdrawal(500)
-# Mickey.make_withdrawal(300)
-# Mickey.display_user_balance()
-
-# Minnie.make_deposit(1500)
-# Minnie.make_withdrawal(1000)
-# Minnie.make_withdrawal(5000)
-# Minnie.make_withdrawal(3000)
-# Minnie.display_user_balance()
-
-# Minnie.transfer_money(500, Mickey)
-
-# example above to work off of for this assignment
+adamAccount.deposit(2000).deposit(400).deposit(50).withdrawl(800).yield_interest().display_account_info()
+jasonAccount.deposit(7000).deposit(10).withdrawl(3400).withdrawl(30).withdrawl(65).withdrawl(800).yield_interest().display_account_info()
 
 
-class User:
-        def __init__(self,name):
-        self.name = name
-        self.amount = 0
+# tried to pull this off of the @classmethod section of the learn platform, but i don't think it works. It didn't break the code.. yay? 
+@classmethod
+def all_balances(cls):
+    sum = 0
+    for balance in cls.all_accounts:
+        sum += balance
+    return sum
+#======================================================================
+# Starting over  ⬆
+#======================================================================
+
+
+
+#======================================================================
+# March Python 2022 failed attempt  ⬇
+#======================================================================
+
+
+
+    # def deposit(self, amount):
+    #     self.amount += amount
+    #     print("You deposited: ", amount)
+
+    # def withdraw(self, amount):
+    #     self.amount -= amount
+    #     print("Withdrawal amount: ", amount)
+
+    # def display_account_info(self):
+    #     print(f"Account Balance: ${balance}")
+
+    # def yield_interest(self):
+    #     self.amount *= amount
+    #     print("Interest Rate: ", amount)
+
+
 
